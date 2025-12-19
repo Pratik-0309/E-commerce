84d7fd1c73f39b4465851c807318d0dd3d528dc6
PratikParate <paratepratik10@gmail.com>
2025-12-19 13:48:11 +0530

    Enhance product filtering by integrating search functionality

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 function Collection() {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilters, setShowFilters] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -32,6 +32,9 @@ function Collection() {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
     if(category.length > 0){
       productsCopy = productsCopy.filter(item => category.includes(item.category))
     }
@@ -60,7 +63,7 @@ function Collection() {
 
   useEffect(()=> {
     applyFilter();
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
 
   useEffect(()=>{
     sortProduct();
