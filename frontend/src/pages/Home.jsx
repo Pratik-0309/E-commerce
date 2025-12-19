aadb3393de98e0afdc24fa1f93d26f79951875db
PratikParate <paratepratik10@gmail.com>
2025-12-19 09:25:37 +0530

    Add Home component layout with Hero, LatestCollection, BestSeller, OurPolicy, and NewsLetterBox

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Hero from '../components/Hero.jsx'
+import LatestCollection from '../components/LatestCollection.jsx'
+import BestSeller from '../components/BestSeller.jsx'
+import OurPolicy from '../components/OurPolicy.jsx'
+import NewsLetterBox from '../components/NewsLetterBox.jsx'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Hero /> 
+      <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
+      <NewsLetterBox />
+    </div>
   )
 }
 
