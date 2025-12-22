3f852051bad1a487d447ccb6ce225580448c3315
PratikParate <paratepratik10@gmail.com>
2025-12-22 09:41:51 +0530

    Add useNavigate for routing logic

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets.js";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -10,6 +11,7 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItem, setCartItem] = useState({});
+  const navigate = useNavigate();
 
   const addtoCart = async (itemId, size) => {
     if(!size){
@@ -83,7 +85,8 @@ const ShopContextProvider = (props) => {
     addtoCart,
     getCartCount,
     updatedQuantity,
-    getCartAmount
+    getCartAmount,
+    navigate
   };
 
   return (
