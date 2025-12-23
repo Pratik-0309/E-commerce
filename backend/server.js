e6fd6e71c01f446b8a6c33e12b6dd2e16d27694b
PratikParate <paratepratik10@gmail.com>
2025-12-23 18:44:09 +0530

    Connect MongoDB to the backend server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js';
 
 const app = express();
+connectDB();
+
 
 app.use(express.json());
 app.use(cors());
