efb7da460d12e497077d5f051ef12865db64db88
PratikParate <paratepratik10@gmail.com>
2025-12-23 21:04:04 +0530

    Define user authentication routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js';
+import userRouter from './routes/userRoute.js'
 
 const app = express();
 connectDB();
@@ -9,6 +10,9 @@ connectDB();
 
 app.use(express.json());
 app.use(cors());
+
+
+app.use('/api/user',userRouter)
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log(`Server is running on port ${process.env.PORT || 8080}  `);
