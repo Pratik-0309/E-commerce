565d7848705aea4addbcfaf069c4b64bffedec1e
PratikParate <paratepratik10@gmail.com>
2025-12-23 18:42:18 +0530

    Setup MongoDB connection with Mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose
+      .connect(`${process.env.MONGODB_URI}`)
+      .then(() => {
+        console.log("MongoDB connected successfully");
+      })
+      .catch((error) => {
+        console.error("MongoDB connection error:", error.message);
+        process.exit(1);
+      });
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
