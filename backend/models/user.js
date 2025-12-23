6c4f5b66ea11ae87dcfde07125b5fffacc6498da
PratikParate <paratepratik10@gmail.com>
2025-12-23 20:59:03 +0530

    Fix Mongoose model argument order error

--- backend/models/user.js
+++ backend/models/user.js
@@ -23,6 +23,6 @@ const userSchema = new mongoose.Schema(
   { minimize: false }
 );
 
-const User = mongoose.model(userSchema, "User");
+const User = mongoose.model("User",userSchema );
 
 export default User;
