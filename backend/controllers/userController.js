432dea66fd5318914267c5362635331535f2107b
PratikParate <paratepratik10@gmail.com>
2025-12-23 21:02:54 +0530

    Initialize user controller function

---
+++ backend/controllers/userController.js
@@ -0,0 +1,15 @@
+import User from "../models/user.js";
+
+const loginUser = async(req,res) => {
+   
+}
+
+const registerUser = async(req,res) => {
+
+}
+
+const adminLogin = async(req,res)=> {
+
+}
+
+export {loginUser, registerUser,adminLogin}
