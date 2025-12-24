4b81f8a77a8d8e889fe605705263111371924026
PratikParate <paratepratik10@gmail.com>
2025-12-24 18:20:47 +0530

    Add auth methods and password middleware

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema(
   {
@@ -23,6 +24,34 @@ const userSchema = new mongoose.Schema(
   { minimize: false }
 );
 
-const User = mongoose.model("User",userSchema );
+// Middleware to hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return null;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// Middleware to compare password
+userSchema.method.matchPassword = async function(enteredPassword){
+  return await bcrypt.compare(enteredPassword,this.password);
+}
+
+userSchema.method.generateAccessToken = function(){
+  return jwt.sign({
+    _id: this._id,
+    email: this.email
+  },process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '15m'
+  })
+}
+
+userSchema.method.generateRefreshToken = function(){
+  return jwt.sign({
+    _id: this._id
+  }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: '7d'
+  })
+}
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
