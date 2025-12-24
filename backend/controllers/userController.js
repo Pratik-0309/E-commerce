638eb8e6304841c9c9589acbba14c7337c371ebe
PratikParate <paratepratik10@gmail.com>
2025-12-24 19:04:07 +0530

    implement user login and register controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,80 @@
 import User from "../models/user.js";
 
-const loginUser = async(req,res) => {
-   
-}
 
-const registerUser = async(req,res) => {
 
-}
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-const adminLogin = async(req,res)=> {
+    if (!name || !email || !password) {
+      return res
+        .status(401)
+        .json({ message: "Please Provide required field." });
+    }
 
-}
+    const userExist = await User.findOne({ email });
 
-export {loginUser, registerUser,adminLogin}
+    if (userExist) {
+      return res.status(401).json({ message: "user already exist." });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    console.log("User Register Successfully : ", user.name)
+
+    return res.status(200).json({
+      user,
+      message: "User Register Successfully",
+    });
+  } catch (error) {
+    console.log("error:", error.message);
+    return res.status(404).json({ message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(401).json({
+        message: "Please enter required fields.",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email",
+      });
+    }
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    const loggedInUser = await User.findById(user._id).select(
+      "-password"
+    )
+
+    return res
+    .status(200)
+    .json({
+      user: loggedInUser
+    })
+
+  } catch (error) {
+    console.log("error: ", error.message);
+    return res.status(500).json({
+      message: error.message
+    })
+  }
+};
+
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
