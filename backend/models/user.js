523bba62e1ff51fe151ece2a27bde5d6f6aea8b6
PratikParate <paratepratik10@gmail.com>
2025-12-25 09:29:28 +0530

    Fix middleware by adding next()

--- backend/models/user.js
+++ backend/models/user.js
@@ -29,6 +29,7 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return null;
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 // Middleware to compare password
