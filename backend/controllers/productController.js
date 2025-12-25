4009303a3a3c44270c8904685144ea6e96c4fda9
PratikParate <paratepratik10@gmail.com>
2025-12-25 10:21:49 +0530

    Initialize product controller functions

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+import Product from '../models/product.js'
+
+const addProduct = async(req,res) => {
+   
+}
+
+const listProduct = async(req,res) => {
+
+}
+
+
+const removeProduct = async(req,res) => {
+
+}
+
+const singleProduct = async(req,res) => {
+    
+}
+
+export {addProduct, listProduct, removeProduct, singleProduct}
