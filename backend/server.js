651e784c975c8c93a901c2a9dd0bad8c94ef58ec
PratikParate <paratepratik10@gmail.com>
2025-12-26 14:43:08 +0530

    configure CORS for multi-origin cookie support

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,32 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/db.js';
-import userRouter from './routes/userRoute.js'
-import productRouter from './routes/productRoute.js'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 connectDB();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
 
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8080}  `);
+});
 
-app.listen(process.env.PORT || 8080, ()=>{
-    console.log(`Server is running on port ${process.env.PORT || 8080}  `);
-    
-})
-
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
