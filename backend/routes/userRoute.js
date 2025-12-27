450bb08e3b8e5d92c316dffccfb252ad6477a0fb
PratikParate <paratepratik10@gmail.com>
2025-12-27 09:10:39 +0530

    add admin-logout route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { loginUser, registerUser, adminLogin } from '../controllers/userController.js';
+import { loginUser, registerUser, adminLogin, adminLogout } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 router.post('/admin-login',adminLogin);
+router.post("/admin-logout",adminLogout);
 
 export default router
