3ac34b02dc925d07a882a7cac360ec868b667c19
PratikParate <paratepratik10@gmail.com>
2025-12-29 16:00:32 +0530

    implements logout functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,10 +2,31 @@ import { assets } from "../assets/assets.js";
 import { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext.jsx";
+import axios from "axios";
 
 function Navbar() {
   const [visible, setVisible] = useState(false);
-  const { setShowSearch, getCartCount } = useContext(ShopContext);
+  const {
+    setShowSearch,
+    getCartCount,
+    backendURL,
+    navigate,
+    isLoggedIn,
+    setIsLoggedIn,
+  } = useContext(ShopContext);
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post(backendURL + "/api/user/logout", {}, { withCredentials: true });
+      if (response.status === 200) {
+        setIsLoggedIn(false);
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
@@ -42,20 +63,24 @@ function Navbar() {
           className="w-5 cursor-pointer"
         />
         <div className="group relative">
-          <Link to='/login'>
             <img
+              onClick={()=> isLoggedIn ? null :  navigate('/login')}
               src={assets.profile_icon}
               className="w-5 cursor-pointer"
               alt=""
-            /> 
-          </Link>
-          <div className="group-hover:block hidden absolute dropdrop-menu right-0 pt-4">
+            />
+          {isLoggedIn && <div className="group-hover:block hidden absolute dropdrop-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded ">
               <p className="cursor-pointer hover:text-black">My Profile</p>
               <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
+              <p
+                onClick={handleLogout}
+                className="cursor-pointer hover:text-black"
+              >
+                Logout
+              </p>
             </div>
-          </div>
+          </div>}
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
