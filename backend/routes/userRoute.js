eb76ddd2db2720d4aa437da4cfcb47d67fb21349
PratikParate <paratepratik10@gmail.com>
2025-12-29 15:58:05 +0530

    add user-logout and refresh access Token route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { loginUser, registerUser, adminLogin, adminLogout } from '../controllers/userController.js';
+import { loginUser, registerUser, adminLogin, adminLogout, logoutUser, refreshAccessToken} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
+router.post('/logout',logoutUser);
+router.post('/refresh-token',refreshAccessToken);
 router.post('/admin-login',adminLogin);
 router.post("/admin-logout",adminLogout);
 
