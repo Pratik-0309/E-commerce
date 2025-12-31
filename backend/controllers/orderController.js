a61e426a6f3d20b7e4dfe8d90b1dc6aa69074b37
PratikParate <paratepratik10@gmail.com>
2025-12-31 19:04:23 +0530

    initialize order controller function to manage orders

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,32 @@
+
+// COD
+const placeOrder = async(req,res) => {
+
+}
+
+// Stripe
+const placeOrderStripe = async(req,res) => {
+    
+}
+
+// Razorpay
+const placeOrderRazorpay = async(req,res) => {
+    
+}
+
+// All orders data for admin 
+const allOrders = async(req,res) => {
+    
+}
+
+// User order details
+const userOrders = async(req,res) => {
+    
+}
+
+// update order Status
+const updateOrderStatus = async(req,res) => {
+    
+}
+
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateOrderStatus };
