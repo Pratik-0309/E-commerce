b2cab5c4b16f8d4d947cf1a6b04630c76088cdb4
PratikParate <paratepratik10@gmail.com>
2026-01-02 10:26:32 +0530

    add mt to logout button

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = ({setIsLoggedIn}) => {
       <img className="w-[max(10%,80px)]" src={assets.logo} alt="" />
       <button 
         onClick={logoutHandler}
-        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 sm:mt-2 rounded-full text-xs
         sm:text-sm"
       >
         Logout
