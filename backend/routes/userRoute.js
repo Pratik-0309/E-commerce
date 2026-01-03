7e7fed193f90360aa34815a5bf6e44d7945b4124
PratikParate <paratepratik10@gmail.com>
2026-01-03 13:09:57 +0530

    add /update route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { loginUser, registerUser, adminLogin, adminLogout, logoutUser, userProfile,refreshAccessToken} from '../controllers/userController.js';
+import { loginUser, registerUser, adminLogin, adminLogout, logoutUser,updateProfile, userProfile,refreshAccessToken} from '../controllers/userController.js';
 import verifyJWT from "../middleware/auth.js"
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/register',registerUser);
 router.post('/login',loginUser);
 router.post('/logout',logoutUser);
 router.get('/profile',verifyJWT,userProfile);
+router.post('/update',verifyJWT,updateProfile);
 router.post('/refresh-token',refreshAccessToken);
 router.post('/admin-login',adminLogin);
 router.post("/admin-logout",adminLogout);
